fe79023c8d53af4aa56d0a18926a6a786bb5d5ba
Harry Marr <harry.marr@gmail.com>
2015-10-08 14:55:24 +0100

    WIP on auto-reversing block form of set_lock_timeout

--- lib/active_record/zero_downtime/command_recorder.rb
+++ lib/active_record/zero_downtime/command_recorder.rb
@@ -1,14 +1,21 @@
 module ActiveRecord
   module ZeroDowntime
     module CommandRecorder
-      def set_lock_timeout(*args)
-        milliseconds = args.first
-        milliseconds ||= DEFAULT_TIMEOUT_MILLIS
-        record(:set_lock_timeout, milliseconds)
+      def set_lock_timeout(timeout_ms = DEFAULT_TIMEOUT_MILLIS)
+        old_timeout_ms = show_lock_timeout
+
+        record(:set_lock_timeout, [old_timeout_ms])
+        yield
+        record(:set_lock_timeout, [timeout_ms])
       end
 
       def invert_set_lock_timeout(args)
         [:set_lock_timeout, args]
+      end
+
+      def show_lock_timeout
+        result = ActiveRecord::Base.connection.execute('SHOW lock_timeout')
+        result.first["lock_timeout"].to_i
       end
     end
   end
--- lib/active_record/zero_downtime/postgresql_adapter.rb
+++ lib/active_record/zero_downtime/postgresql_adapter.rb
@@ -1,8 +1,21 @@
 module ActiveRecord
   module ZeroDowntime
     module PostgreSQLAdapter
-      def set_lock_timeout(milliseconds = DEFAULT_TIMEOUT_MILLIS)
-        execute("SET lock_timeout = #{milliseconds}")
+      def set_lock_timeout(timeout_ms = DEFAULT_TIMEOUT_MILLIS)
+        old_timeout_ms = show_lock_timeout
+        execute("SET lock_timeout = #{timeout_ms}")
+
+        if block_given?
+          begin
+            yield
+          ensure
+            execute("SET lock_timeout = #{old_timeout_ms}")
+          end
+        end
+      end
+
+      def show_lock_timeout
+        execute('SHOW lock_timeout').first["lock_timeout"].to_i
       end
     end
   end
