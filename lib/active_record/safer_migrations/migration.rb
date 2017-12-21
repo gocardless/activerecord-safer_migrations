88d0e7369e4e3ed142e3e74cd95e29c9e1dde09b
Tim Rogers <tim@gocardless.com>
2017-12-21 16:09:21 +0000

    Fix Metrics/LineLength offences

--- lib/active_record/safer_migrations/migration.rb
+++ lib/active_record/safer_migrations/migration.rb
@@ -21,9 +21,12 @@ module ActiveRecord
         def exec_migration(conn, direction)
           # lock_timeout is an instance accessor created by class_attribute
           lock_timeout_ms = lock_timeout || SaferMigrations.default_lock_timeout
-          statement_timeout_ms = statement_timeout || SaferMigrations.default_statement_timeout
+          statement_timeout_ms = statement_timeout || SaferMigrations.
+            default_statement_timeout
           SettingHelper.new(conn, :lock_timeout, lock_timeout_ms).with_setting do
-            SettingHelper.new(conn, :statement_timeout, statement_timeout_ms).with_setting do
+            SettingHelper.new(conn,
+                              :statement_timeout,
+                              statement_timeout_ms).with_setting do
               super(conn, direction)
             end
           end
@@ -48,10 +51,10 @@ module ActiveRecord
 
         def set_statement_timeout(timeout)
           if timeout.zero?
-            raise "Setting statement_timeout to 0 is dangerous - it disables the statement " \
-                  "timeout rather than instantly timing out. If you *actually* " \
-                  "want to disable the statement timeout (not recommended!), use the " \
-                  "`disable_statement_timeout!` method."
+            raise "Setting statement_timeout to 0 is dangerous - it disables the " \
+                  "statement timeout rather than instantly timing out. If you " \
+                  "*actually* want to disable the statement timeout (not recommended!)" \
+                  ", use the `disable_statement_timeout!` method."
           end
           self.statement_timeout = timeout
         end
